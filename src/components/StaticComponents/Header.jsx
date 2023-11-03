39650afc116688b4f94318052677ee6b5cadbe44
abrahambelaygelaw <abrahambelaygelaw@gmail.com>
2023-11-04 01:51:15 +0300

    fix search bar

--- src/components/StaticComponents/Header.jsx
+++ src/components/StaticComponents/Header.jsx
@@ -1,52 +1,75 @@
-import React ,{useContext,useEffect}from 'react'
-import { Outlet, useNavigate  } from 'react-router-dom'
-import { allContext } from '../../Constants/allContext'
+import React, { useContext, useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { allContext } from "../../Constants/allContext";
 
 const Header = () => {
-  const {query,setQuery,theme,setTheme} = useContext(allContext)
-  const navigate = useNavigate()
-  const toggleTheme = () =>{
-    const curr = localStorage.getItem("theme")
-    localStorage.setItem("theme",curr==="light"?"dark":"light")
-    setTheme(localStorage.getItem("theme"))
-  }
-  document.body.className = theme
-  
+  const { query, setQuery, theme, setTheme } = useContext(allContext);
+  const navigate = useNavigate();
+  const toggleTheme = () => {
+    const curr = localStorage.getItem("theme");
+    localStorage.setItem("theme", curr === "light" ? "dark" : "light");
+    setTheme(localStorage.getItem("theme"));
+  };
+  document.body.className = theme;
+
   useEffect(() => {
-    window.addEventListener("keyup",handleEnter);
-    return ()=>removeEventListener("keyup",handleEnter)
-  })
-  const handleEnter = (e) =>{
-    if(e.key=="Enter"){
-      toggleSearch()
+    window.addEventListener("keyup", handleEnter);
+    return () => removeEventListener("keyup", handleEnter);
+  });
+  const handleEnter = (e) => {
+    if (e.key == "Enter") {
+      toggleSearch();
     }
-  }
-  const toggleSearch = () =>{
-    if (query){
-      navigate(`/search/photos/${query.toLowerCase().replace(/\s/g, '-')}`)
+  };
+  const toggleSearch = () => {
+    if (query) {
+      navigate(`/search/photos/${query.toLowerCase().replace(/\s/g, "-")}`);
     }
-  }
-  
+  };
 
   return (
     <>
-      <div className='header sub-background'>
-        <div className="logo-search" >
-          <div className='logo'>
-            <img src="https://i.pinimg.com/564x/6b/f7/b9/6bf7b9879abf86c73603922e3000b79d.jpg" alt="" onClick={()=>{navigate('/home')}}/>
+      <div className="header sub-background">
+        <div className="logo-search">
+          <div className="logo">
+            <img
+              src="https://i.pinimg.com/564x/6b/f7/b9/6bf7b9879abf86c73603922e3000b79d.jpg"
+              alt=""
+              onClick={() => {
+                navigate("/home");
+              }}
+            />
           </div>
-          <div className='search-bar'>
-            <input className='main-background' type="text" placeholder='Search'
-          onChange={(e)=>{
-            setQuery(prev=>e.target.value)}}/>
-            <i className="fa-solid fa-magnifying-glass main-background" onClick={()=>{toggleSearch()}}></i>
-            <button className='main-background' onClick={()=>{toggleTheme()}}>{theme=="light"?"Dark":"Light"} Mode</button>
+          <div className="search-bar">
+            <input
+              className="main-background"
+              type="text"
+              placeholder="Search"
+              value={query}
+              onChange={(e) => {
+                setQuery(e.target.value);
+              }}
+            />
+            <i
+              className="fa-solid fa-magnifying-glass main-background"
+              onClick={() => {
+                toggleSearch();
+              }}
+            ></i>
+            <button
+              className="main-background"
+              onClick={() => {
+                toggleTheme();
+              }}
+            >
+              {theme == "light" ? "Dark" : "Light"} Mode
+            </button>
           </div>
         </div>
-    </div>
-    <Outlet/>
+      </div>
+      <Outlet />
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
